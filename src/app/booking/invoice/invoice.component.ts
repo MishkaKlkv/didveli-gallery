1bbccc09b9ebf9343dbb5fcb25814f9d90305a85
M.Klokov <klokov.mixan@gmail.com>
2022-11-30 15:06:23 +0400

    invoice printing fixes

--- src/app/booking/invoice/invoice.component.ts
+++ src/app/booking/invoice/invoice.component.ts
@@ -45,10 +45,11 @@ export class InvoiceComponent implements OnInit {
 
   print() {
 
-    // window.print();
+    console.log(document.getElementById('invoice'))
+    window.print();
 
-
-    // const printContent = document.getElementById("invoice");
+    // const printContent = document.getElementById('invoice');
+    // console.log(printContent);
     // const WindowPrt = window.open('', '', 'left=0,top=0,width=900,height=900,toolbar=0,scrollbars=0,status=0');
     // WindowPrt.document.write(printContent.innerHTML);
     // WindowPrt.document.close();
